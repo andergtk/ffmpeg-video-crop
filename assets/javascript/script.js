0ff87bd519cdf0a3a0f40e6f2b7f14c74c6abcc2
Anderson Camargo <andergtk@gmail.com>
2024-07-04 19:19:25 -0300

    update timeline current time on video update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,23 +4,46 @@ const app = createApp({
   data() {
     return {
       file: null,
+      videoInfo: {},
     }
   },
+
   computed: {
-    videoUrl() {
+    fileUrl() {
       if (!this.file) return null
       const url = window.URL || window.webkitURL
       return url.createObjectURL(this.file)
     },
+
+    durationToDisplay() {
+      return this.secondsToDisplay(this.videoInfo.duration)
+    },
+
+    currentTimeToDisplay() {
+      return this.secondsToDisplay(this.videoInfo.currentTime)
+    },
+
     enableDeleteClip() {
       return false
     },
   },
+
   methods: {
     onFileUpload(event) {
       this.file = event.target.files[0]
     },
-  }
+
+    onVideoTimeUpdate(event) {
+      this.videoInfo = {
+        duration: event.target.duration,
+        currentTime: event.target.currentTime,
+      }
+    },
+
+    secondsToDisplay(seconds = 0) {
+      return new Date(seconds * 1000).toISOString().slice(14, 23)
+    },
+  },
 })
 
 app.mount('#app')
