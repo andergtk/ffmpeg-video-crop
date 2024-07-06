94a20472e4f3680b22069a690c006b9d7bc8a8ef
Anderson Camargo <andergtk@gmail.com>
2024-07-06 04:04:37 -0300

    add repo as base path for builds

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'docs',
-    emptyOutDir: true,
-  },
+  base: '/ffmpeg-video-crop/',
   plugins: [
     vue(),
   ],
